25f9ba1eff40d3376dad44e663f353f9be533b68
Nahuel701 <60718922+Nahuel701@users.noreply.github.com>
2024-11-23 18:07:43 -0300

    -

--- competencia_2/run_0/909_run_orden227.r
+++ competencia_2/run_0/909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/competencia_2/run_0/990_workflow_orden227_SEMI.r"
+PARAM <- "competencia_2/run_0/990_workflow_orden227_SEMI.r"
 
 envg <- env()
 
