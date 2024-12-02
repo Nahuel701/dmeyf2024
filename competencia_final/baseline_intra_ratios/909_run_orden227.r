06903a178ba9fa3da3e7bbc4b49f3c94bbb9b9f8
Nahuel701 <60718922+Nahuel701@users.noreply.github.com>
2024-12-01 21:29:13 -0300

    -

--- competencia_final/baseline_intra_ratios/909_run_orden227.r
+++ competencia_final/baseline_intra_ratios/909_run_orden227.r
@@ -2,7 +2,7 @@ require("rlang")
 
 
 # workflow que voy a correr
-PARAM <- "/competencia_final/baseline_intra_adds/990_workflow_orden227_SEMI.r"
+PARAM <- "/competencia_final/baseline_intra_logs/990_workflow_orden227_SEMI.r"
 
 envg <- env()
 
